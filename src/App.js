0cd20f5bd2608b8a7c65683b52cbab6208b3a76e
Umang Kahndelwal <umang@vahak.in>
2021-04-12 13:47:27 +0530

    fix: test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 4 <code>src/App.js</code> and save to reload.
+          Edit 5 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,36 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register({
-  onUpdate: (reg) => {
-    const t1= Date.now();
-      console.log("New update avalaible");
+  onUpdate: async (reg) => {
+    const t1 = Date.now();
+    console.log("New update avalaible");
+    const cache = await caches.open("v-cache");
+    const oldVal = (await (await cache.match("test")).json());
+    await cache.put("test", new Response(Number(oldVal)++))
+    const shouldUpdate = confirm("Update");
+    if(shouldUpdate){
       const registrationWaiting = reg.waiting;
       if (registrationWaiting) {
-          registrationWaiting.postMessage({ type: "SKIP_WAITING" });
-          registrationWaiting.addEventListener("statechange", (e) => {
-              if ((e.target )?.state === "activated") {
-    const t2= Date.now();
-    console.log("Reloading now to get the latest version", t2-t1);
-                  // window.location.reload();
-              }
-          });
+        registrationWaiting.postMessage({ type: "SKIP_WAITING" });
+        registrationWaiting.addEventListener("statechange", (e) => {
+          if (e.target?.state === "activated") {
+            const t2 = Date.now();
+            console.log("Reloading now to get the latest version", t2 - t1);
+            
+            window.location.reload();
+          }
+        });
       }
-  }
+    }else {
+      window.localStorage.setItem("ua", "1")
+    }
+  },
 });
 
 // If you want to start measuring performance in your app, pass a function

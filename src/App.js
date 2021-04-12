8d1613bf8e9fe606f6683400fd8e74358ed05d2f
Umang Kahndelwal <umang@vahak.in>
2021-04-12 16:24:13 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {  useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 function App() {
+  const [swReg, setSwReg] = useState();
+
+  const getNewUpdate = () => {
+    const regWaiting = swReg?.waiting;
+    if (regWaiting) {
+      regWaiting.postMessage({ type: "SKIP_WAITING" });
+      regWaiting.addEventListener("statechange", (e) => {
+        if (e.target?.state === "activated") {
+          // const t2 = Date.now();
+          // console.log("Reloading now to get the latest version", t2 - t1);
+          window.location.reload();
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const newVal = process.env.REACT_APP_V;
+      const cache = await caches.open("v-cache");
+      if (window.localStorage.getItem("ua")) {
+        const shouldUpdate = window.confirm("install update?");
+        if (shouldUpdate) {
+          getNewUpdate();
+          await cache?.put("test", new Response(newVal));
+          window.localStorage.removeItem("ua");
+        } else {
+          window.localStorage.setItem("ua", "1");
+        }
+      } else {
+        const oldVal = await (await cache?.match("test"))?.json();
+        if (oldVal !== newVal) {
+          const shouldUpdate = window.confirm("install update?");
+          if (shouldUpdate) {
+            getNewUpdate();
+            await cache?.put("test", new Response(newVal));
+            window.localStorage.removeItem("ua");
+          } else {
+            window.localStorage.setItem("ua", "1");
+          }
+        }
+      }
+    })();
+
+    serviceWorkerRegistration.register({
+      onSuccess: (reg) => {
+        setSwReg(reg);
+        console.log("reg", reg);
+      },
+      //   // onUpdate: async (reg) => {
+      //   //   const t1 = Date.now();
+      //   //   console.log("New update avalaible");
+      //   //   const cache = await caches.open("v-cache");
+      //   //   const oldVal = (await (await cache?.match("test"))?.json());
+      //   //   await cache?.put("test", new Response(oldVal ? Number(oldVal) +1 : 1))
+      //   //   const shouldUpdate = window.confirm("Update");
+      //   //   if(shouldUpdate){
+      //   //     const registrationWaiting = reg.waiting;
+      //   //     if (registrationWaiting) {
+      //   //       registrationWaiting.postMessage({ type: "SKIP_WAITING" });
+      //   //       registrationWaiting.addEventListener("statechange", (e) => {
+      //   //         if (e.target?.state === "activated") {
+      //   //           const t2 = Date.now();
+      //   //           console.log("Reloading now to get the latest version", t2 - t1);
+
+      //   //           window.location.reload();
+      //   //         }
+      //   //       });
+      //   //     }
+      //   //   }else {
+      //   //     window.localStorage.setItem("ua", "1")
+      //   //   }
+      //   // },
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 6 <code>src/App.js</code> and save to reload.
+          Edit 8 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +93,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,34 +15,14 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: async (reg) => {
-    const t1 = Date.now();
-    console.log("New update avalaible");
-    const cache = await caches.open("v-cache");
-    const oldVal = (await (await cache.match("test")).json());
-    await cache.put("test", new Response(oldVal ? Number(oldVal) +1 : 1))
-    const shouldUpdate = window.confirm("Update");
-    if(shouldUpdate){
-      const registrationWaiting = reg.waiting;
-      if (registrationWaiting) {
-        registrationWaiting.postMessage({ type: "SKIP_WAITING" });
-        registrationWaiting.addEventListener("statechange", (e) => {
-          if (e.target?.state === "activated") {
-            const t2 = Date.now();
-            console.log("Reloading now to get the latest version", t2 - t1);
-            
-            window.location.reload();
-          }
-        });
-      }
-    }else {
-      window.localStorage.setItem("ua", "1")
-    }
-  },
-});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// serviceWorkerRegistration.register({
+//   onSuccess: (reg) => {
+//     console.log("reg",reg)
+//   }})

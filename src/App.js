dc79a9920c661e054358e11576fb81d51e107faf
Umang Kahndelwal <umang@vahak.in>
2021-04-12 17:33:59 +0530

    test

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 11 <code>src/App.js</code> and save to reload.
+          Edit 12 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"

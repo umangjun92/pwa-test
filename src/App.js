7c844b346e71d21d929ec568d5cbbfb35f56d490
Umang Kahndelwal <umang@vahak.in>
2021-04-15 17:02:48 +0530

    a

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ function App() {
           <>
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Edit 41 <code>src/App.js</code> and save to reload.
+              Edit 42 <code>src/App.js</code> and save to reload.
             </p>
             <a
               className="App-link"
